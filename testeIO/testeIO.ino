4d2ec90be10d18de615205d54a4122deedf30036
Valdemir 《DSW project》 <134114016+Valdemir-DSW@users.noreply.github.com>
2025-03-24 14:42:41 -0300

    Add files via upload

---
+++ testeIO/testeIO.ino
@@ -0,0 +1,34 @@
+const int numAnalog = 6;  // Número de portas analógicas
+const int numDigital = 14; // Número de portas digitais
+const int digitalPins[numDigital] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A6, A7};
+
+void setup() {
+    Serial.begin(9600);
+    for (int i = 0; i < numDigital; i++) {
+        pinMode(digitalPins[i], INPUT_PULLUP);
+    }
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        char command = Serial.read();
+        if (command == 'R') {
+            // Leitura das portas analógicas
+            for (int i = 0; i < numAnalog; i++) {
+                Serial.print(map(analogRead(i), 0, 1023, 0, 255));
+                Serial.print(",");
+            }
+
+            // Leitura das portas digitais
+            for (int i = 0; i < numDigital; i++) {
+                Serial.print(digitalRead(digitalPins[i]) == HIGH ? 255 : 0);
+                if (i < numDigital - 1) Serial.print(",");
+            }
+
+            Serial.println();
+        }
+    }
+    // Coloque um pequeno delay para dar tempo ao microcontrolador de realizar outras tarefas
+    delay(10);
+}
+
